c17547b4439a4703a80b7a2d97ee7583e3371871
Imre Péntek <pentek.imre@gmail.com>
2023-11-03 19:05:21 +0100

    whitespace fix

--- sslstuff.h
+++ sslstuff.h
@@ -3,7 +3,7 @@
 
 class QString;
 
-bool generateX509(                                const QString &certFileName, const QString &keyFileName, long daysValid=10*365, unsigned int length_in_bits=2048);
+bool generateX509(                                  const QString &certFileName, const QString &keyFileName,              long daysValid=10*365, unsigned int length_in_bits=2048);
 bool genkey(const QString &fqdn, const QString &ca, const QString &certFileName, const QString &keyFileName, long serial, long daysValid=10*365, unsigned int length_in_bits=2048);
 
 #endif // INCLUDED_SSLSTUFF_H
